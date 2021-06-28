fcc22c857a631a3f1bab16208dc8af4ffb0f0dde
Ayan Sarkar <ayansarkar367@gmail.com>
2021-06-28 05:36:17 +0530

    plot component vs time graph with cmd data

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,100 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Bar } from 'react-chartjs-2';
+import { TextField, MenuItem, styled } from '@material-ui/core';
+
+import { emd } from '../emd';
+
+const MyTextField = styled(TextField)({
+  width: '100%',
+});
+
+function getKey() {
+  let times = [];
+  for (const obj of emd) {
+    times.push(obj.Time);
+  }
+  return times;
+}
+
+function getvalue(attr) {
+  let list = [];
+  for (const obj of emd) {
+    list.push(obj[attr]);
+  }
+  return list;
+}
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      attribute: '',
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      attribute: e.target.value,
+    });
+  };
+
   render() {
-    console.log('PROPS', this.props);
-    return <div></div>;
+    const attr = this.state.attribute;
+    const opts = ['PM1', 'PM2.5', 'PM10', 'NO2', 'CO2', 'CO', 'Hum', 'Temp'];
+    const data = {
+      labels: getKey(),
+      datasets: [
+        {
+          label: attr,
+          backgroundColor: 'rgba(75,192,192,1)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderWidth: 2,
+          data: getvalue(attr),
+        },
+      ],
+    };
+    return (
+      <div className="App">
+        <form className="form">
+          <div className="input-field">
+            <MyTextField
+              select
+              label="Component"
+              value={attr}
+              onChange={this.handleChange}
+              helperText="Select component"
+              variant="outlined"
+            >
+              {opts.map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </MyTextField>
+          </div>
+        </form>
+        <div className="results">
+          <div className="graph-title">{attr ? attr : 'component'} vs time</div>
+          <div className="bar-graph">
+            <Bar
+              data={data}
+              options={{
+                title: {
+                  display: true,
+                  text: 'Average Rainfall per month',
+                  fontSize: 20,
+                },
+                legend: {
+                  display: true,
+                  position: 'right',
+                },
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
