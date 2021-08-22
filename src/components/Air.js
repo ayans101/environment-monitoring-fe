979b0f6c3c7e95a3f1229203d961cd778d12092e
ayans101 <78583131+ayans101@users.noreply.github.com>
2021-08-22 23:15:23 +0530

    call api for weather data and  make bar charts

---
+++ src/components/Air.js
@@ -0,0 +1,148 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Bar } from 'react-chartjs-2';
+import { TextField, MenuItem, styled } from '@material-ui/core';
+
+import { emd } from '../helper/emd';
+import { ems } from '../helper/ems_nov';
+
+const MyTextField = styled(TextField)({
+  width: '100%',
+});
+
+function getEmsKey(attr) {
+  let dates = [];
+  for (const date in ems[attr]) {
+    dates.push(date);
+  }
+  return dates;
+}
+
+function getEmsValue(attr) {
+  let values = [];
+  for (const date in ems[attr]) {
+    values.push(ems[attr][date]);
+  }
+  return values;
+}
+
+function getKey() {
+  let times = [];
+  for (const obj of emd) {
+    times.push(obj.Time);
+  }
+  return times;
+}
+
+function getValue(attr) {
+  let list = [];
+  for (const obj of emd) {
+    list.push(obj[attr]);
+  }
+  return list;
+}
+
+class Air extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataType: '',
+      attribute: '',
+    };
+  }
+
+  handleInputChange = (field, value) => {
+    this.setState({
+      ...this.state,
+      [field]: value,
+    });
+  };
+  render() {
+    const attr = this.state.attribute;
+    const data_opts = ['ems', 'emd'];
+    const com_opts =
+      this.state.dataType === 'emd'
+        ? ['PM1', 'PM2.5', 'PM10', 'NO2', 'CO2', 'CO', 'Hum', 'Temp']
+        : ['Nitrogen Dioxide', 'Carbon Monoxide'];
+    const ems_data = {
+      labels: getEmsKey(attr),
+      datasets: [
+        {
+          label: attr,
+          backgroundColor: 'rgba(75,192,192,1)',
+          borderColor: 'rgba(75,192,192,1)',
+          // borderWidth: 2,
+          data: getEmsValue(attr),
+        },
+      ],
+    };
+    const emd_data = {
+      labels: getKey(),
+      datasets: [
+        {
+          label: attr,
+          backgroundColor: 'rgba(75,192,192,1)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderWidth: 2,
+          data: getValue(attr),
+        },
+      ],
+    };
+    return (
+      <div className="aqm">
+        <form className="form">
+          <div className="input-field">
+            <MyTextField
+              select
+              label="Data"
+              value={this.state.dataType}
+              onChange={(e) =>
+                this.handleInputChange('dataType', e.target.value)
+              }
+              helperText="Select Data"
+              variant="outlined"
+            >
+              {data_opts.map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </MyTextField>
+          </div>
+          <div className="input-field">
+            <MyTextField
+              select
+              label="Component"
+              value={attr}
+              onChange={(e) =>
+                this.handleInputChange('attribute', e.target.value)
+              }
+              helperText="Select component"
+              variant="outlined"
+            >
+              {com_opts.map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </MyTextField>
+          </div>
+        </form>
+        <div className="results">
+          <div className="graph-title">{attr ? attr : 'component'} vs time</div>
+          <div className="bar-graph">
+            <Bar data={this.state.dataType === 'emd' ? emd_data : ems_data} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    data: state.data,
+  };
+}
+
+export default connect(mapStateToProps)(Air);
---
+++ src/components/Weather.js
@@ -0,0 +1,145 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Bar } from 'react-chartjs-2';
+import { TextField, styled } from '@material-ui/core';
+
+const MyTextField = styled(TextField)({
+  width: '100%',
+});
+
+function getValue(weather_data, attribute) {
+  let list = [];
+  if (!weather_data) {
+    return list;
+  }
+  for (const obj of weather_data) {
+    list.push(obj[attribute]);
+  }
+  return list;
+}
+
+function getBar(weather_data, attribute) {
+  const data = {
+    labels: [
+      '12:00 AM',
+      '01:00 AM',
+      '02:00 AM',
+      '03:00 AM',
+      '04:00 AM',
+      '05:00 AM',
+      '06:00 AM',
+      '07:00 AM',
+      '08:00 AM',
+      '09:00 AM',
+      '10:00 AM',
+      '11:00 AM',
+      '12:00 PM',
+      '01:00 PM',
+      '02:00 PM',
+      '03:00 PM',
+      '04:00 PM',
+      '05:00 PM',
+      '06:00 PM',
+      '07:00 PM',
+      '08:00 PM',
+      '09:00 PM',
+      '10:00 PM',
+      '11:00 PM',
+    ],
+    datasets: [
+      {
+        label: attribute,
+        backgroundColor: 'rgba(75,192,192,1)',
+        borderColor: 'rgba(75,192,192,1)',
+        // borderWidth: 2,
+        data: getValue(weather_data, attribute),
+      },
+    ],
+  };
+  return data;
+}
+
+class Weather extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: undefined,
+      data: undefined,
+    };
+  }
+
+  handleInputChange = async (field, value) => {
+    const url = `http://127.0.0.1:8000/get-weather-data/${value}`;
+    const response = await fetch(url);
+    if (response.status === 200) {
+      const data = await response.json();
+      this.setState({
+        [field]: value,
+        data: data,
+      });
+    } else {
+      this.setState({
+        [field]: value,
+        data: undefined,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="weather">
+        <div className="input-field">
+          <MyTextField
+            type="date"
+            label="Enter Date"
+            variant="outlined"
+            // defaultValue="2017-05-24"
+            InputLabelProps={{ shrink: true }}
+            value={this.state.date}
+            onChange={(e) => this.handleInputChange('date', e.target.value)}
+          />
+        </div>
+        <div className="bars">
+          <div className="bar-graph">
+            <Bar data={getBar(this.state.data, 'humidity')} />
+          </div>
+          <div className="bar-graph">
+            <Bar data={getBar(this.state.data, 'wind_deg')} />
+          </div>
+          <div className="bar-graph">
+            <Bar data={getBar(this.state.data, 'wind_speed')} />
+          </div>
+          <div className="bar-graph">
+            <Bar data={getBar(this.state.data, 'clouds')} />
+          </div>
+          <div className="bar-graph">
+            <Bar data={getBar(this.state.data, 'temp')} />
+          </div>
+          <div className="bar-graph">
+            <Bar data={getBar(this.state.data, 'temp_feel')} />
+          </div>
+          <div className="bar-graph">
+            <Bar data={getBar(this.state.data, 'pressure')} />
+          </div>
+          <div className="bar-graph">
+            <Bar data={getBar(this.state.data, 'dew_point')} />
+          </div>
+          <div className="bar-graph">
+            <Bar data={getBar(this.state.data, 'uvi')} />
+          </div>
+          <div className="bar-graph">
+            <Bar data={getBar(this.state.data, 'visibility')} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    data: state.data,
+  };
+}
+
+export default connect(mapStateToProps)(Weather);
